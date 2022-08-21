3cf23009148ce6da14939ee5745ff0bd57c929e2
kienhoang255 <hoangtrungkien255@gmail.com>
2022-08-21 20:24:05 +0700

    push_drag_to_scroll_stories_list

--- src/components/Story/StoryList.jsx
+++ src/components/Story/StoryList.jsx
@@ -1,19 +1,74 @@
-import { Grid, Wrap } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import StoryItem from "./StoryItem";
 import { stories } from "../../utils/db";
+import { style } from "../../assets/css/stye.css";
 
 export default function StoryList({ id }) {
   const _id = id;
   const filterId = stories.filter((story) => story.idUser === _id);
-  // console.log(filterId);
+  const $ = document.querySelector.bind(document)
+  const [state, setState] = useState("")
+  const [isDown, setIsDown] = useState(false)
+  const [startX, setStartX] = useState("")
+  const [scrollLeft, setScrollLeft] = useState("")
+  const [x, setX] = useState("")
+  const [walk, setWalk] = useState("")
+
+
+  const onPointerEvent = (e) => {
+    const slider = $('#btn-wrapper')
+    if (e.type === "pointerdown") {
+      slider.classList.add("active");
+      console.log("nam")
+    }
+    else {
+      slider.classList.remove("active");
+      console.log("tha")
+    }
+  }
+
+  const handleOnMouse = (event) => {
+    const slider = $('#wrapper')
+    if (event.type === "mousedown") {
+      setIsDown(true)
+      slider.classList.add("active")
+      setStartX(event.pageX - slider.offsetLeft);
+      setScrollLeft(slider.scrollLeft)
+      // console.log(startX)
+
+    } else if (event.type === "mouseup") {
+      setIsDown(false)
+      slider.classList.remove("active")
+      // console.log("tha")
+
+    } else if (event.type === "mouseleave") {
+      setIsDown(false)
+      slider.classList.remove("active")
+      // console.log("leave")
+
+    } else {
+      if (!isDown) return // dừng event
+      // console.log("mouseMove")
+      event.preventDefault();
+      setX(event.pageX - slider.offsetLeft)
+      setWalk(x - startX);
+      slider.scrollLeft = scrollLeft - walk;
+      // console.log({ x, startX })
+      // console.log(walk)
+      slider.scrollLeft = scrollLeft - walk;
+    }
+  }
+
   return (
-    <Wrap p="2em" pl="2em" minHeight="10em">
-      <Grid templateColumns="repeat(7, 1fr)" gap={7}>
+    <Box id="btn-wrapper" /*onPointerDown={onPointerEvent}*/ >
+      {/* <input type="button" value="<" class="leftBtn" /> */}
+      <Box p="2em" pl="2em" minHeight="10em" id="wrapper" onMouseDown={handleOnMouse} onMouseUp={handleOnMouse} onMouseMove={handleOnMouse} onMouseLeave={handleOnMouse} >
         {filterId.map((element, index) => (
           <StoryItem key={index} item={element} />
         ))}
-      </Grid>
-    </Wrap>
-  );
+      </Box>
+      {/* <input type="button" value=">" class="rightBtn" /> */}
+    </Box>
+  )
 }
